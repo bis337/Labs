ed66981ead8d2f04a01dcb51b7fff66ea43e201a
isb22 <isb22@tpu.ru>
2025-11-20 11:34:57 +0700

    feature(GCD): добавлена начальная реализация НОД и запись ввода в файл
    - добавлена логика вычисления НОД методом перебора
    - добавлена запись введённых пользователем чисел в файл input.txt
    
    ВАЖНОЕ ИЗМЕНЕНИЕ: Добавлена запись ввода в файл input.txt. Теперь программа зависит от файловой системы.

--- LB3(6.19)/Program.cs
+++ LB3(6.19)/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace LB3_6._19_
 {
@@ -15,17 +12,23 @@ namespace LB3_6._19_
             long num1 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Введите второе число");
             long num2 = Convert.ToInt64(Console.ReadLine());
+
+            // Запись в файл
+            string inputPath = "input.txt";
+            using (StreamWriter writer = new StreamWriter(inputPath))
+            {
+                writer.WriteLine(num1);
+                writer.WriteLine(num2);
+            }
+
             long i = 1;
             long largeComDiv = 0;
-            while (i < Math.Max(num1, num2))
+            while (i <= Math.Min(Math.Abs(num1), Math.Abs(num2)))
             {
-
-
                 if (num1 % i == 0 && num2 % i == 0)
                 {
                     largeComDiv = i;
                 }
-                //Console.WriteLine(i);
                 i++;
             }
             Console.WriteLine($"Наибольший общий делитель: {largeComDiv}");
